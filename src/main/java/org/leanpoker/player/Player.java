e97f2eb851c06344479fbd6452b3d10972c17f85
Sandor Balazsi <sandor.balazsi@ericsson.com>
2018-09-21 10:18:35 +0200

    fdsfsdf

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,7 +8,7 @@ public class Player {
 
     public static int betRequest(final JsonElement request) {
         System.out.println(request);
-        return 8;
+        return 100;
     }
 
     public static void showdown(final JsonElement game) {
