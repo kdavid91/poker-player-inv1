369f3f6776594c790561616bb72855bbb3bb497e
Sandor Balazsi <sandor.balazsi@ericsson.com>
2018-09-21 10:07:21 +0200

    dfsadsa

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,10 +7,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(final JsonElement request) {
-<<<<<<< HEAD
         System.out.println(request);
-=======
->>>>>>> 8e143cb71e17c0c85eb51db2e93f90f4c1bea0d2
         return 8;
     }
 
