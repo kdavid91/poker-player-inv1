e9ef389cae88e2a1b5273c98c80ac65d0a539a51
ezkunda <david.kun@ericsson.com>
2018-09-21 11:31:56 +0200

    Suit function

--- src/main/java/org/leanpoker/player/Hand.java
+++ src/main/java/org/leanpoker/player/Hand.java
@@ -2,11 +2,15 @@ package org.leanpoker.player;
 
 public class Hand {
 
-    public static boolean isPair(Card left, Card right) {
+    public static boolean isPair(final Card left, final Card right) {
         return left.getRank().equals(right.getRank());
     }
 
-    public static boolean isHighPair(Card left, Card right) {
+    public static boolean isSameSuit(final Card left, final Card right) {
+        return left.getSuit().equals(right.getSuit());
+    }
+
+    public static boolean isHighPair(final Card left, final Card right) {
         return isPair(left, right) && isHighCard(left) && isHighCard(right);
     }
 
