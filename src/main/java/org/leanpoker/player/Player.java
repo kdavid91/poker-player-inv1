f5a0ccd0932a5ef0b72003eade38271dc0b53a7e
ezkunda <david.kun@ericsson.com>
2018-09-21 09:59:43 +0200

    comment1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,7 +7,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(final JsonElement request) {
-        return 8;
+        return 4;
     }
 
     public static void showdown(final JsonElement game) {
